dbcbce480311b6d872ee91973fd2977a28dc2f71
shahidnasiri <shahidnasiri4017@gmail.com>
2023-11-06 00:35:35 -0800

    second commit

---
+++ backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,77 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Patients </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Patient</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">View Patients</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Patients</a>
+                                    </li>
+                                   
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Employees </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">Add Employee</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_employee.php">View Employees</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Manage Employees</a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="his_admin_assaign_dept.php">Assign Department</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+                           
+
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
